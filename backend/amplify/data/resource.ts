183ae746e7dd1178c758e19bb39232ff3cface60
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-10-29 03:09:25 -0400

    feat: added purchase function that works with stripe

--- backend/amplify/data/resource.ts
+++ backend/amplify/data/resource.ts
@@ -1,4 +1,5 @@
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { purchase } from "../functions/purchase/resource";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -7,6 +8,15 @@ specifies that any unauthenticated user can "create", "read", "update",
 and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
+  purchase: a
+    .query()
+    .arguments({
+      name: a.string(),
+    })
+    .returns(a.string())
+    .handler(a.handler.function(purchase))
+    .authorization((allow) => [allow.guest()]),
+
   Todo: a
     .model({
       content: a.string(),
@@ -19,7 +29,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "iam",
   },
 });
 
