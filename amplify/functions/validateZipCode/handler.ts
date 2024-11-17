052554d30dfb4ee2f58e32b6083ff5d98199524d
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2024-11-16 19:34:23 -0500

    fix: typo

--- amplify/functions/validateZipCode/handler.ts
+++ amplify/functions/validateZipCode/handler.ts
@@ -1,12 +1,12 @@
 import type { Handler } from 'aws-lambda';
-const ZIP_CODE_ARRAY = ['1001', '11356', '11368', '11709'];
+const ZIP_CODE_ARRAY = ['11001', '11356', '11368', '11709'];
 
 export const handler: Handler = async (event, context) => {
-  const { zipcode } = event.arguments;
+  const { zipCode } = event.arguments;
 
-  console.log('zipcode', zipcode, context);
-  if (!ZIP_CODE_ARRAY.includes(zipcode)) {
-    return { error: `Zipcode ${zipcode} not found` };
+  console.log('zipcode', zipCode, context);
+  if (!ZIP_CODE_ARRAY.includes(zipCode)) {
+    return { error: `Zipcode ${zipCode} not found` };
   }
   const returnParams = { success: true };
   return JSON.stringify(returnParams);
